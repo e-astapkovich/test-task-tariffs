f5a99c1d91aeeaf57fb19819a9f6be960c104645
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2024-05-02 00:16:52 +0500

    Создал контроллер TariffController

---
+++ controllers/admin/TariffController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controllers\admin;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use app\models\Tariff;
+
+class TariffController extends Controller
+{
+    public function actionIndex()
+    {
+        $tariffs = Tariff::find()->all();
+        return $this->render('admin/tarif/index', [
+            'tariffs' => $tariffs,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+    }
+
+    public function actionEdit($id)
+    {
+    }
+
+    public function actionDelete($id)
+    {
+    }
+}
